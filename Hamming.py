34edcf9db5e48a8af74a88f5523cf1bf9d34951b
Cheshire <857531567@qq.com>
2016-07-02 05:52:31 +0800

    make Hamming code table

--- Hamming.py
+++ Hamming.py
@@ -1,4 +1,21 @@
 # -*- coding:utf-8 -*-
+X = [[0,0,0,0],
+     [0,0,0,1],
+     [0,0,1,0],
+     [0,0,1,1],
+     [0,1,0,0],
+     [0,1,0,1],
+     [0,1,1,0],
+     [0,1,1,1],
+     [1,0,0,0],
+     [1,0,0,1],
+     [1,0,1,0],
+     [1,0,1,1],
+     [1,1,0,0],
+     [1,1,0,1],
+     [1,1,1,0],
+     [1,1,1,1]]
+
 G = [[1, 0, 0, 0, 0, 1, 1],
      [0, 1, 0, 0, 1, 0, 1],
      [0, 0, 1, 0, 1, 1, 0],
@@ -16,31 +33,58 @@ E = [[1, 0, 0, 0, 0, 0, 0],
      [0, 0, 0, 0, 0, 1, 0],
      [0, 0, 0, 0, 0, 0, 1]]
 
-def error_flag(file_name):
-    #initiate error flag
+
+def make_code_table(output_file_name):
+    try:
+        # initiate 
+        C = list()
+        for i in range(len(X)):
+            ci = str()
+            for j in range(len(E)):
+                num = 0
+                for k in range(len(G)):
+                    if(X[i][k]==G[k][j])and(X[i][k]!=0) : 
+                        num = 1
+                        break
+                ci = ci + str(num)
+            C.append(ci)
+        # check, not necessary
+        for line in C: print line
+        # write output-file
+        fout = open(output_file_name, "w+")
+        for i in range(len(C)):
+            s2 = str()
+            for j in range(len(G)):
+                s2 = s2 + str(X[i][j])
+            fout.write("%s %s\n"%(s2, C[i]))
+        fout.close()
+
+    except Exception, e:
+        raise e
+
+
+def error_flag(output_file_name):
+    # initiate error flag
     S = list()
     for i in range(len(E)):
-        si = list()
+        si = str()
         for j in range(len(H)):
             num = 0
             for k in range(len(E)):
                 if(E[i][k]==H[j][k])and(E[i][k]!=0) : 
                     num = 1
                     break
-            si.append(num)
+            si = si + str(num)
         S.append(si)
     # check, not necessary
     for line in S: print line
     # write output-file
-    f = open(file_name, "w+")
+    f = open(output_file_name, "w+")
     for i in range(len(E)):
-    	s1 = str()
-    	s2 = str()
-        for j in range(len(H)):
-            s1 = s1 + str(S[i][j])
+        s2 = str()
         for j in range(len(E)):
             s2 = s2 + str(E[i][j])
-        f.write("%s %s\n"%(s1, s2))
+        f.write("%s %s\n"%(S[i], s2))
     f.close()
 
 
@@ -49,3 +93,4 @@ def error_flag(file_name):
 #test if function is correct
 if __name__ == "__main__":
     error_flag("error_flag.txt")
+    make_code_table("hamming_table_ouy.txt")
